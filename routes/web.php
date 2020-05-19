b04322eda5bcc23036169bf544b72e660503bdaa
vagrant <vagrant@homestead>
2020-05-19 08:18:42 +0000

    Added a few test starter views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $name = request('name');
+    return $name;
+    return view('test', [
+        'name' == $name
+    ]);
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/test', function() {
+    return view('test');
+});
+
+Route::get('/posts/{post}', function($post) {
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post!',
+        'my-second-post' => 'Now I am getting the hand of this blogging thing.',
+        'my-third-post' => 'Hey, Ive almost created my own CMS!'
+    ];
+    return view('post', [
+        'post' => $posts[$post]
+    ]);
 });
