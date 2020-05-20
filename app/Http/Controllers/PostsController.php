efea81e9b86fa8af14fe69585e4054a3c739247c
Jonathan Perry <jpper@umich.edu>
2020-05-20 12:01:13 +0900

    Went back and finished database access hello eloquent section

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,24 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use DB;
+use App\Post;
 
 class PostsController extends Controller
 {
-    public function show($post) {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post!',
-            'my-second-post' => 'Now I am getting the hand of this blogging thing.',
-            'my-third-post' => 'Hey, Ive almost created my own CMS!'
-        ];
+    public function show($slug) {
+        $post = Post::where('slug', $slug)->firstOrFail();
 
-        if (!array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not foudn');
+        if (! $post) {
+            abort(404);
         }
 
         return view('post', [
-            'post' => $posts[$post] ?? 'Nothing here yet.'
+            'post' => $post
         ]);
     }
 }
