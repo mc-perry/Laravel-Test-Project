9492f39592edeaf6e2bb3dc1e53b623dfee1a9a9
Jonathan Perry <jpper@umich.edu>
2020-05-21 00:22:45 +0900

    Update database connection

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +65,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('postgres://ojktjmmtxmwxmb:c47e7a7efd6412b2c98b4e6ef142cd722c7b47f21a6ea2e4090bb278cbde7ccb@ec2-52-87-135-240.compute-1.amazonaws.com:5432/db9ab66bphbim6'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
